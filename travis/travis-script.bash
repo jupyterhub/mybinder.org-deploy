bb9945ea3a9af4be0bda975cad836b30d79d8468
yuvipanda <yuvipanda@gmail.com>
2017-09-28 16:25:09 -0700

    Install google-cloud-sdk
    The bash installer is interactive & hence not useful

--- travis/travis-script.bash
+++ travis/travis-script.bash
@@ -5,8 +5,13 @@ set -euo pipefail
 
 echo "Starting deploy..."
 
+
 openssl aes-256-cbc -K $encrypted_510e3970077d_key -iv $encrypted_510e3970077d_iv -in travis/deploy-secrets.tar.gz.enc -out travis/deploy-secrets.tar.gz -d
 tar xvf travis/deploy-secrets.tar.gz
+
+# Install Google Cloud SDK
+export PATH=${HOME}/google-cloud-sdk/bin:${PATH}
+gcloud components install kubectl
 
 gcloud auth activate-service-account --key-file=travis/deploy-secrets/google-auth-key.json
 
