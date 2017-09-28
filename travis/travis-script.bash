89d69f843a0338e99f529cf83a674529a9335e97
yuvipanda <yuvipanda@gmail.com>
2017-09-28 16:55:05 -0700

    Move installing kubectl to pre deploy

--- travis/travis-script.bash
+++ travis/travis-script.bash
@@ -9,9 +9,8 @@ echo "Starting deploy..."
 openssl aes-256-cbc -K $encrypted_510e3970077d_key -iv $encrypted_510e3970077d_iv -in travis/deploy-secrets.tar.gz.enc -out travis/deploy-secrets.tar.gz -d
 tar xvf travis/deploy-secrets.tar.gz
 
-# Install Google Cloud SDK
+# Set up the gcloud SDK for use!
 export PATH=${HOME}/google-cloud-sdk/bin:${PATH}
-gcloud components install kubectl
 
 gcloud auth activate-service-account --key-file=travis/deploy-secrets/google-auth-key.json
 
