1fdb5b593603fcb528111c9ec775d71da6e8edca
yuvipanda <yuvipanda@gmail.com>
2017-09-28 16:19:10 -0700

    Initial .travis.yml + secrets encrypted

---
+++ travis/travis-script.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+# Used by travis to trigger deployments
+# Keeping this here rather than make travis.yml too complex
+
+echo "Starting deploy..."
+
+openssl aes-256-cbc -K $encrypted_510e3970077d_key -iv $encrypted_510e3970077d_iv -in travis/deploy-secrets.tar.gz.enc -out travis/deploy-secrets.tar.gz -d
+tar xvf travis/deploy-secrets.tar.gz
+
+gcloud auth activate-service-account --key-file=travis/deploy-secrets/google-auth-key.json
+
+echo "Done!"
