2dd9c901417bae5a430ebd174595d9426d7c9a83
yuvipanda <yuvipanda@gmail.com>
2017-09-28 17:48:39 -0700

    Install git-crypt & unlock the repo

--- travis/travis-script.bash
+++ travis/travis-script.bash
@@ -9,15 +9,9 @@ echo "Starting deploy..."
 openssl aes-256-cbc -K $encrypted_510e3970077d_key -iv $encrypted_510e3970077d_iv -in travis/deploy-secrets.tar.gz.enc -out travis/deploy-secrets.tar.gz -d
 tar xvf travis/deploy-secrets.tar.gz
 
-# Set up the gcloud SDK for use!
-export PATH=${HOME}/google-cloud-sdk/bin:${PATH}
-
 gcloud auth activate-service-account --key-file=deploy-secrets/google-auth-key.json
 gcloud container clusters get-credentials binder-hub --zone=us-central1-a --project=binder-testing
 
-kubectl version
-kubectl --namespace=kube-system get pod
-helm init --client-only
-helm version
+git-crypt unlock deploy-secrets/crypt-key
 
 echo "Done!"
