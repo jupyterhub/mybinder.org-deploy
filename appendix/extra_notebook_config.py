329ab6a52c272cf425f9bd69221bf072893f87b1
Min RK <benjaminrk@gmail.com>
2018-03-15 11:07:55 +0100

    make internal-cull configurable by env
    allows us to change parameters without rebuilding all images

--- appendix/extra_notebook_config.py
+++ appendix/extra_notebook_config.py
@@ -5,11 +5,19 @@ c.NotebookApp.jinja_template_vars.update({
 })
 
 # shutdown notebook after no activity for 10 minutes
-c.NotebookApp.shutdown_no_activity_timeout = 10 * 60
-# shutdown idle kernels after 10 minutes
-c.MappingKernelManager.cull_idle_timeout = 10 * 60
-# check kernel idleness every minute
-c.MappingKernelManager.cull_interval = 60
+if os.getenv('CULL_TIMEOUT'):
+    # shutdown the server after no activity
+    c.NotebookApp.shutdown_no_activity_timeout = int(os.getenv('CULL_TIMEOUT'))
+
+if os.getenv('CULL_KERNEL_TIMEOUT'):
+    # shutdown kernels after no activity
+    c.MappingKernelManager.cull_idle_timeout = int(os.getenv('CULL_KERNEL_TIMEOUT'))
+
+if os.getenv('CULL_INTERVAL'):
+    # check for idle kernels this often
+    c.MappingKernelManager.cull_interval = int(os.getenv('CULL_INTERVAL'))
+
 # a kernel with open connections but no activity still counts as idle
 # this is what allows us to shutdown servers when people leave a notebook open and wander off
-c.MappingKernelManager.cull_connected = True
+if os.getenv('CULL_CONNECTED') not in {'', '0'}:
+    c.MappingKernelManager.cull_connected = True
