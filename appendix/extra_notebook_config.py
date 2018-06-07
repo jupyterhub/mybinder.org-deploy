28a8153612807f52e1152324e9b1aade38b32bb5
Min RK <benjaminrk@gmail.com>
2018-06-07 14:00:46 +0200

    appendix: trust proxy headers

--- appendix/extra_notebook_config.py
+++ appendix/extra_notebook_config.py
@@ -26,3 +26,6 @@ if os.getenv('CULL_INTERVAL'):
 # this is what allows us to shutdown servers when people leave a notebook open and wander off
 if os.getenv('CULL_CONNECTED') not in {'', '0'}:
     c.MappingKernelManager.cull_connected = True
+
+# trust proxy headers
+c.NotebookApp.trust_xheaders = True
