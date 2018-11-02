2e757e9ade139edc0a3c7c3ad699c50de6343e3f
yuvipanda <yuvipanda@gmail.com>
2018-11-02 10:07:57 -0700

    Read events from stackdriver & post them to GCS
    This should be superseeded soon, since stackdriver API calls
    are slow & restricted to 1/s projectwide.

--- images/events-archiver/events-archiver.py
+++ images/events-archiver/events-archiver.py
@@ -6,7 +6,8 @@ import time
 import json
 import argparse
 from dateutil.parser import parse
-from google.cloud import logging
+from google.cloud import logging, storage
+import tempfile
 
 
 def fetch_events(project, log_name, start_time, end_time):
@@ -18,8 +19,11 @@ def fetch_events(project, log_name, start_time, end_time):
 
     for page in client.list_entries(filter_=filter).pages:
         for entry in page:
-            yield json.loads(entry.payload['message'])
-        time.sleep(10)
+            yield entry.payload['message']
+        # API limit is 1 read request per second for whole project
+        # We sleep 5s between requests to be nicer
+        # https://cloud.google.com/logging/quotas
+        time.sleep(5)
 
 def main():
     argparser = argparse.ArgumentParser()
@@ -28,7 +32,7 @@ def main():
         help='Name of the GCP project to read logs from'
     )
     argparser.add_argument(
-        'log-name',
+        'log_name',
         help='Name of log to read from'
     )
 
@@ -42,13 +46,45 @@ def main():
         help='Read events emitted during & until this timestamp' 
     )
 
+    argparser.add_argument(
+        'bucket_name',
+        help='GCS bucket to archive events to'
+    )
+
+    argparser.add_argument(
+        'object_name',
+        help='Name of GCS object to archive to'
+    )
+
     args = argparser.parse_args()
 
-    for event in fetch_events(
-        'binder-prod', 'binderhub-events-text',
-        parse(args.start_time).isoformat() + 'Z', parse(args.end_time).isoformat() + 'Z'
-    ):
-        print(event)
+    count = 0
+    with tempfile.TemporaryFile(mode='w') as f:
+        for event in fetch_events(
+            args.project, args.log_name,
+            parse(args.start_time).isoformat() + 'Z', parse(args.end_time).isoformat() + 'Z'
+        ):
+            f.write(event + '\n')
+            count += 1
+            if count % 100 == 0:
+                print(f'Wrote {count} lines')
+
+
+        print(f'Total {count} lines written')
+
+        f.flush()
+        f.seek(0)
+
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(args.bucket_name)
+        blob = bucket.blob(args.object_name)
+        blob.upload_from_file(f)
+
+        print(f'File uploaded to {args.bucket_name}/{args.object_name}')
+
+
+
+
 
 if __name__ == '__main__':
     main()
