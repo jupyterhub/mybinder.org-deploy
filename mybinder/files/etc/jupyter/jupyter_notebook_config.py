4774a4cb8df77b91fe52398cb4d04d9cee8b6cd3
Tim Head <betatim@gmail.com>
2019-11-08 07:32:12 +0100

    Enable translation Jinja extension for old notebooks

--- mybinder/files/etc/jupyter/jupyter_notebook_config.py
+++ mybinder/files/etc/jupyter/jupyter_notebook_config.py
@@ -1,5 +1,16 @@
+import notebook
 import os
+
+from distutils.version import LooseVersion as V
+
+
 c.NotebookApp.extra_template_paths.append('/etc/jupyter/templates')
+
+
+# For old notebook versions we have to explicitly enable the translation
+# extension
+if V(notebook.__version__) < V('5.1.0'):
+    c.NotebookApp.jinja_environment_options = {'extensions': ['jinja2.ext.i18n']}
 
 
 def make_federation_url(url):
