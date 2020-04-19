b521654fa5cba230c14d664e3d9197858a17937c
Erik Sundell <erik.i.sundell@gmail.com>
2020-04-19 12:55:30 +0200

    Refactor away a kubernetes api request

--- deploy.py
+++ deploy.py
@@ -218,21 +218,14 @@ def deploy(release):
 
     # Explicitly wait for all deployments and daemonsets to be fully rolled out
     print(BOLD + GREEN + f"Waiting for all deployments and daemonsets in {release} to be ready" + NC, flush=True)
-    deployments = subprocess.check_output([
+    deployments_and_daemonsets = subprocess.check_output([
         'kubectl',
         '--namespace', release,
-        'get', 'deployments',
+        'get', 'deployments,daemonsets',
         '-o', 'name'
     ]).decode().strip().split('\n')
 
-    daemonsets = subprocess.check_output([
-        'kubectl',
-        '--namespace', release,
-        'get', 'daemonsets',
-        '-o', 'name'
-    ]).decode().strip().split('\n')
-
-    for d in deployments + daemonsets:
+    for d in deployments_and_daemonsets:
         subprocess.check_call([
             'kubectl', 'rollout', 'status',
             '--namespace', release,
