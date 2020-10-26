6f27e0252decad3b633b6f6bf67bdb55b7773ce3
Sarah Gibson <sgibson@turing.ac.uk>
2020-10-26 13:01:52 +0000

    Add get cluster roles permission

--- terraform/modules/mybinder/variables.tf
+++ terraform/modules/mybinder/variables.tf
@@ -31,6 +31,7 @@ variable "deployer_permissions" {
     "container.apiServices.delete",
     "container.apiServices.get",
     "container.apiServices.list",
+    "container.clusterRoles.get",
     "container.clusters.create",
     "container.clusters.get",
     "container.clusters.getCredentials",
