ff01bf596b516e5990905c7a31e0bb572d3df227
Min RK <benjaminrk@gmail.com>
2022-05-30 15:24:44 +0200

    note about unfinished reports on suspicious dind processes

--- mybinder/files/minesweeper/minesweeper.py
+++ mybinder/files/minesweeper/minesweeper.py
@@ -368,11 +368,10 @@ async def node_report(pods=None, userid=1000):
                     print(f"Failed to kill {proc}: {e}")
             elif proc.suspicious:
                 print(f"dind process is suspicious: {proc}")
-        # FIXME: flake8 detected suspicious_dind_procs_without_pod to not be
-        #        used, it seems like something partially implemented.
-        suspicious_dind_procs_without_pod = [
-            p for p in procs_without_pod if p.suspicious
-        ]
+        # TODO: find a way to identity the build repo responsible for suspicious processes in dind
+        # suspicious_dind_procs_without_pod = [
+        #     p for p in procs_without_pod if p.suspicious
+        # ]
 
     if report_futures:
         await asyncio.gather(*report_futures)
