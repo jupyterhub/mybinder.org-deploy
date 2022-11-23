136a181c8ab2a4c20a0c0f1e5c7a2c647cbc8ae3
Min RK <benjaminrk@gmail.com>
2022-11-23 10:26:01 +0100

    add r2-120 user node pool

--- terraform/ovh/main.tf
+++ terraform/ovh/main.tf
@@ -111,6 +111,30 @@ resource "ovh_cloud_project_kube_nodepool" "user" {
   name         = "user-202211"
   # r2-60 is 4 core, 60GB
   flavor_name   = "r2-60"
+  max_nodes     = 1
+  min_nodes     = 1
+  autoscale     = true
+  template {
+    metadata {
+      labels = {
+        "mybinder.org/pool-type" = "users"
+      }
+    }
+  }
+  lifecycle {
+    ignore_changes = [
+      # don't interfere with autoscaling
+      desired_nodes
+    ]
+  }
+}
+
+resource "ovh_cloud_project_kube_nodepool" "user-a" {
+  service_name = local.service_name
+  kube_id      = ovh_cloud_project_kube.cluster.id
+  name         = "user-202211a"
+  # r2-120 is 8 core, 120GB
+  flavor_name   = "r2-120"
   max_nodes     = 6
   min_nodes     = 1
   autoscale     = true
