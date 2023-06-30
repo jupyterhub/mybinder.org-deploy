604d5921503c8efe26c7f49dd9e13cce59afd451
Simon Li <orpheus+devel@gmail.com>
2023-06-30 22:17:17 +0100

    Some tfsec fixes

--- terraform/aws/curvenote/security-groups.tf
+++ terraform/aws/curvenote/security-groups.tf
@@ -3,18 +3,20 @@
 resource "aws_security_group" "worker_group_all" {
   name_prefix = "worker_group_all_ports"
   vpc_id      = module.vpc.vpc_id
-
+  description = "Allow all ports for worker group"
 
   ingress {
-    protocol  = "-1"
-    from_port = 0
-    to_port   = 0
-    self      = true
+    description = "Allow all inbound traffic"
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    self        = true
   }
   egress {
-    protocol  = "-1"
-    from_port = 0
-    to_port   = 0
+    description = "Allow all outbound traffic"
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
     # self      = true
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -24,11 +26,13 @@ resource "aws_security_group" "worker_group_all" {
 resource "aws_security_group" "all_worker_mgmt" {
   name_prefix = "all_worker_management"
   vpc_id      = module.vpc.vpc_id
+  description = "Worker nodes internal access"
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
 
     cidr_blocks = [
       "10.0.0.0/8",
