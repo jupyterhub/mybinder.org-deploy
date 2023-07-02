7a98cf2451d57dfccbb0a3bf7e2734f61a762880
Simon Li <orpheus+devel@gmail.com>
2023-07-02 17:05:25 +0100

    aws/curvenote: Comment out outputs

--- terraform/aws/curvenote/outputs.tf
+++ terraform/aws/curvenote/outputs.tf
@@ -1,38 +1,18 @@
-output "cluster_id" {
-  description = "EKS cluster ID."
-  value       = module.eks.cluster_id
-}
+# output "cluster_name" {
+#   description = "Kubernetes Cluster Name"
+#   value       = var.cluster_name
+# }
 
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane."
-  value       = module.eks.cluster_endpoint
-}
+# output "cluster_oidc_issuer_url" {
+#   description = "The URL on the EKS cluster for the OpenID Connect identity provider"
+#   value       = module.eks.cluster_oidc_issuer_url
+# }
 
-output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane."
-  value       = module.eks.cluster_security_group_id
-}
+# data "tls_certificate" "cluster_tls_certificate" {
+#   url = module.eks.cluster_oidc_issuer_url
+# }
 
-output "region" {
-  description = "AWS region"
-  value       = var.region
-}
-
-output "cluster_name" {
-  description = "Kubernetes Cluster Name"
-  value       = var.cluster_name
-}
-
-output "cluster_oidc_issuer_url" {
-  description = "The URL on the EKS cluster for the OpenID Connect identity provider"
-  value       = module.eks.cluster_oidc_issuer_url
-}
-
-data "tls_certificate" "cluster_tls_certificate" {
-  url = module.eks.cluster_oidc_issuer_url
-}
-
-output "cluster_tls_certificate_sha1_fingerprint" {
-  description = "The SHA1 fingerprint of the public key of the cluster's certificate"
-  value       = data.tls_certificate.cluster_tls_certificate.certificates[*].sha1_fingerprint
-}
+# output "cluster_tls_certificate_sha1_fingerprint" {
+#   description = "The SHA1 fingerprint of the public key of the cluster's certificate"
+#   value       = data.tls_certificate.cluster_tls_certificate.certificates[*].sha1_fingerprint
+# }
