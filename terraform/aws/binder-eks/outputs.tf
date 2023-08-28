2d4c439c75c3f12281ceec813ddc4a6d75bd4b53
Simon Li <orpheus+devel@gmail.com>
2023-08-28 23:16:23 +0100

    binder-eks: uncomment outputs

--- terraform/aws/binder-eks/outputs.tf
+++ terraform/aws/binder-eks/outputs.tf
@@ -1,21 +1,21 @@
-# output "cluster_name" {
-#   description = "Kubernetes Cluster Name"
-#   value       = var.cluster_name
-# }
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = var.cluster_name
+}
 
-# output "cluster_oidc_issuer_url" {
-#   description = "The URL on the EKS cluster for the OpenID Connect identity provider"
-#   value       = module.eks.cluster_oidc_issuer_url
-# }
+output "cluster_oidc_issuer_url" {
+  description = "The URL on the EKS cluster for the OpenID Connect identity provider"
+  value       = module.eks.cluster_oidc_issuer_url
+}
 
-# data "tls_certificate" "cluster_tls_certificate" {
-#   url = module.eks.cluster_oidc_issuer_url
-# }
+data "tls_certificate" "cluster_tls_certificate" {
+  url = module.eks.cluster_oidc_issuer_url
+}
 
-# output "cluster_tls_certificate_sha1_fingerprint" {
-#   description = "The SHA1 fingerprint of the public key of the cluster's certificate"
-#   value       = data.tls_certificate.cluster_tls_certificate.certificates[*].sha1_fingerprint
-# }
+output "cluster_tls_certificate_sha1_fingerprint" {
+  description = "The SHA1 fingerprint of the public key of the cluster's certificate"
+  value       = data.tls_certificate.cluster_tls_certificate.certificates[*].sha1_fingerprint
+}
 
 output "cluster_endpoint" {
   description = "The endpoint for the EKS Kubernetes API"
