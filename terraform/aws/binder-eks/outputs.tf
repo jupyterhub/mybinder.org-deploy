02cb10fb9f377b7ab9dcecfc46a2cc965edabd4a
Simon Li <orpheus+devel@gmail.com>
2023-08-28 22:26:02 +0100

    Add outputs to binder-eks module

--- terraform/aws/binder-eks/outputs.tf
+++ terraform/aws/binder-eks/outputs.tf
@@ -16,3 +16,17 @@
 #   description = "The SHA1 fingerprint of the public key of the cluster's certificate"
 #   value       = data.tls_certificate.cluster_tls_certificate.certificates[*].sha1_fingerprint
 # }
+
+output "cluster_endpoint" {
+  description = "The endpoint for the EKS Kubernetes API"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_ca_certificate" {
+  description = "The EKS certificate authority data"
+  value       = base64decode(module.eks.cluster_certificate_authority_data)
+}
+output "eks_token" {
+  description = "The EKS token"
+  value       = data.aws_eks_cluster_auth.binderhub.token
+}
