f48c4f79ca05fbddebad14b79ac010e25519afce
Simon Li <orpheus+devel@gmail.com>
2023-09-22 14:38:48 +0100

    Remove aws_s3_bucket_server_side_encryption_configuration from bootstrap

--- terraform/aws/curvenote/bootstrap/backend.tf
+++ terraform/aws/curvenote/bootstrap/backend.tf
@@ -36,15 +36,6 @@ resource "aws_s3_bucket" "bucket" {
   bucket = var.state_bucket_name
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
-  bucket = aws_s3_bucket.bucket.id
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
 resource "aws_s3_bucket_versioning" "bucket_versioning" {
   bucket = aws_s3_bucket.bucket.id
   versioning_configuration {
