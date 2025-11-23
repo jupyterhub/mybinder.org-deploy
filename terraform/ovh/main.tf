cf99693884394ab6d13f4fefe42d93526c747919
Simon Li <orpheus+devel@gmail.com>
2025-11-23 11:19:44 +0000

    ovh terraform s32 backend: update for 1.6.0

--- terraform/ovh/main.tf
+++ terraform/ovh/main.tf
@@ -5,16 +5,21 @@ terraform {
       version = "2.8.0"
     }
     harbor = {
-      source = "goharbor/harbor"
+      source  = "goharbor/harbor"
       version = "3.11.2"
     }
   }
   # store state on gcs, like other clusters
   backend "s3" {
-    bucket                      = "mybinder-2i2c-tf-state"
-    key                         = "terraform.tfstate"
-    region                      = "us-east-va"
-    endpoint                    = "https://s3.us-east-va.io.cloud.ovh.us"
+    bucket = "mybinder-2i2c-tf-state"
+    key    = "terraform.tfstate"
+    region = "us-east-va"
+    # Terraform 1.6.0
+    # https://github.com/hashicorp/terraform/issues/33983#issuecomment-1801632383
+    endpoints = {
+      s3 = "https://s3.us-east-va.io.cloud.ovh.us"
+    }
+    skip_requesting_account_id  = true
     skip_credentials_validation = true
     skip_region_validation      = true
   }
