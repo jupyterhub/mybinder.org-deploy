64808ca9c2c9437c8590d7f8921cf478d99d14e9
Min RK <benjaminrk@gmail.com>
2025-12-10 13:47:26 -0800

    add bids-ovh vm

---
+++ terraform/ovh/bids-ovh.tfvars
@@ -0,0 +1,13 @@
+
+endpoint = "ovh-us"
+
+service_name = "5e4c805d3c614a1085d7b7bc1fee46d6"
+region = "US-EAST-VA"
+zone = "US-EAST-VA-1"
+
+name = "bids-ovh"
+vm = {
+    # flavor and image: see lookup_flavor_image.py
+    flavor_id = "6f5e7aef-5e1d-47e4-ad85-8b235cc2bd00" # b3-64
+    image_id = "03302136-e20b-446e-8b27-cf0aaf161810" #"Ubuntu 24.04"
+}
---
+++ terraform/ovh/vm.tf
@@ -0,0 +1,38 @@
+data "local_file" "ssh_public_key" {
+  filename = "${path.module}/../../secrets/${var.name}.key.pub"
+}
+
+resource "ovh_cloud_project_ssh_key" "vm" {
+  service_name = var.service_name
+  name  = "${var.name}-ssh"
+  public_key = data.local_file.ssh_public_key.content
+}
+
+resource "ovh_cloud_project_instance" "vm" {
+  service_name   = var.service_name
+  region         = var.zone
+  name = "mybinder-${var.name}"
+  billing_period = "hourly"
+  boot_from {
+    image_id = var.vm.image_id
+  }
+  flavor {
+    flavor_id = var.vm.flavor_id
+  }
+  ssh_key {
+    name = ovh_cloud_project_ssh_key.vm.name
+  }
+  network {
+    public = true
+  }
+  
+  auto_backup {
+    cron = "0 23 * * *"
+    rotation = 7
+  }
+  # ignore image id update, which can change
+  # and would require recreating the vm
+  lifecycle {
+    ignore_changes = [boot_from]
+  }
+}
