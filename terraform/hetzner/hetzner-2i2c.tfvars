9bca5fe7a2cda19ba47f54174f51933b2814aca0
Min RK <benjaminrk@gmail.com>
2026-03-11 16:09:46 -0700

    configure push mirrors via harbor
    secrets must be applied via ui

--- terraform/hetzner/hetzner-2i2c.tfvars
+++ terraform/hetzner/hetzner-2i2c.tfvars
@@ -1,3 +1,11 @@
 state_s3_endpoint = "https://nbg1.your-objectstorage.com"
-name = "hetzner-2i2c"
-registry_users = ["hetzner-2i2c", "hetzner-gesis", "gesis-harbor"]
+name              = "hetzner-2i2c"
+registry_users    = ["hetzner-2i2c", "hetzner-gesis", "gesis-harbor"]
+push_mirrors = {
+  gesis = {
+    name      = "gesis"
+    url       = "https://registry.gesis.mybinder.org"
+    access_id = "robot$mybinder-builds+2i2c-harbor-builder"
+    # add secret via ui
+  }
+}
--- terraform/hetzner/variables.tf
+++ terraform/hetzner/variables.tf
@@ -14,19 +14,31 @@ variable "name" {
 }
 
 variable "registry_quota_gb" {
-  type = number
-  default = 10000
+  type        = number
+  default     = 10000
   description = <<-EOT
   harbor registry project quota size in gigabytes
   EOT
 }
 
 variable "registry_users" {
-  type = set(string)
-  default = []
+  type        = set(string)
+  default     = []
   description = <<-EOT
   harbor registry users
   One builder (push/pull) and one puller (pull-only) robot account
   will be created for each name listed here.
+  EOT
+}
+
+variable "push_mirrors" {
+  type = map(object({
+    name : string,
+    url : string,
+    access_id : string,
+  }))
+  default     = {}
+  description = <<-EOT
+  harbor registries to mirror to
   EOT
 }
